e8c1b390a23f83783e65b69a58b3401588bb4667
paulocarpinetti <pauloscarpinetti@gmail.com>
2022-11-19 00:43:12 -0300

    Primeiros testes

---
+++ 05_firstSteps/RestWhithSpringBootJava18/src/main/java/com/betocode/RestWhithSpringBootJava18Application.java
@@ -0,0 +1,13 @@
+package com.betocode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWhithSpringBootJava18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWhithSpringBootJava18Application.class, args);
+	}
+
+}
